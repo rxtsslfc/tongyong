a466fa6c7b6a62938ac1ef2535d14bdaf7f4742a
Sebastian Ene <sebastianene@google.com>
2024-08-16 11:06:09 +0000

    ANDROID: KVM: arm64: Handle guest FF-A ID_GET call
    Handle the get id FF-A guest call in the hypervisor and return the id to
    the caller. pKVM uses the vm handle index as an id returned to FF-A.
    
    Bug: 269285339
    Bug: 278749606
    Change-Id: I3dc7ff9ace92750edc1581e212422904d9c0cb13
    Signed-off-by: Sebastian Ene <sebastianene@google.com>

--- arch/arm64/kvm/hyp/nvhe/ffa.c
+++ arch/arm64/kvm/hyp/nvhe/ffa.c
@@ -1099,6 +1099,9 @@ bool kvm_guest_ffa_handler(struct pkvm_hyp_vcpu *hyp_vcpu, u64 *exit_code)
 	case FFA_FN64_MEM_LEND:
 		ret = __do_ffa_mem_xfer(FFA_FN64_MEM_LEND, &res, ctxt, vm_handle, exit_code);
 		break;
+	case FFA_ID_GET:
+		ffa_to_smccc_res_prop(&res, FFA_RET_SUCCESS, vm_handle);
+		break;
 	default:
 		if (ffa_call_supported(func_id))
 			goto unhandled;
